17185ae58f9674d40c692de570ec8a487707418f
Samuel Maiko <sammymaiko721@gmail.com>
2024-09-02 17:11:31 +0300

    why mamapesa section done

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../common/NavBar";
+import WhyChooseMamaPesa from "../WhyChooseMamaPesa/WhyChooseMamaPesa";
 
 const LandingPage = () => {
   return (
     <div>
       <NavBar />
+      <WhyChooseMamaPesa />
     </div>
   );
 };
