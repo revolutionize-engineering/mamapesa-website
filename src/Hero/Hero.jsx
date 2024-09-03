b401c84f7a3537758f6c961a4d714e1ea95a4091
rugzzz <kinoroangela29@gmail.com>
2024-09-03 19:31:32 +0300

    Updated Hero Section

---
+++ src/Hero/Hero.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { CheckCircle } from '@phosphor-icons/react';
+import ImageHero from '../assets/ImageHero.jpg';
+
+const Hero = () => {
+  return (
+    /*Background Image*/
+    <div className="relative w-full min-h-96 h-full ">
+        <img
+            src={ImageHero}
+            alt='Background'
+            className='hidden lg:block w-full object-cover '
+        />        
+
+    <main className="absolute top-8 left-5 flex flex-col lg:flex-row items-center lg:justify-between mt-10 p-6 bg-opacity-80 bg-white shadow-lg rounded-lg max-w-4xl">
+        <div>
+            <h1 className="text-4xl font-montserrat font-bold mb-6">Unlocking Financial Inclusion For Women in Africa </h1>
+            <ul className="list-none space-y-6">
+                <li className="flex items-center">
+                    <CheckCircle size={32} className="mr-2 fill-green-500" />
+                    <span className="font-semibold text-lg">Personalized financial guidance using mamapesa chatbot
+                    </span>
+                </li>
+
+                <li className="flex items-center">
+                    <CheckCircle size={32} className="mr-2 fill-green-500" />
+                    <span className="font-semibold text-lg">Loans at 0% interest rate</span>
+                </li>
+
+                <li className="flex items-center">
+                    <CheckCircle size={32} className="mr-2 fill-green-500" />
+                    <span className="font-semibold text-lg">Goal oriented asset savings </span>
+                </li>
+
+                <li className="flex items-center">
+                    <CheckCircle size={32} className="mr-2 fill-green-500" />
+                    <span className="font-semibold text-lg">Group savings  </span>
+                </li>
+
+                <li className="flex items-center">
+                    <CheckCircle size={32} className="mr-2 fill-green-500" />
+                    <span className="font-semibold text-lg">Additional services   </span>
+                </li>
+            </ul>
+
+            <button className="font-montserrat font-semibold text-white py-2 px-4 my-6 rounded hover:bg-[#7a2713] bg-[#571C0E]" >
+                View our Services
+            </button>
+        </div>
+
+    </main>
+    </div>
+  )
+}
+
+export default Hero
